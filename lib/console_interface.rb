df55c7ed5344352ace16246952894b2685bb9cee
lordsynergy <djordj88@mail.ru>
2022-12-25 18:33:45 +0300

    add gem colorize

--- lib/console_interface.rb
+++ lib/console_interface.rb
@@ -1,61 +1,41 @@
 class ConsoleInterface
-  # В константе FIGURES будут лежать все текстовые файлы из папки figures,
-  # помещённые в массив. Один элемент массива — одна строка с содержимым целого
-  # файла.
   FIGURES =
     Dir["#{__dir__}/../data/figures/*.txt"].
     sort.
     map { |file_name| File.read(file_name) }
 
-  # На вход конструктор класса ConsoleInterface принимает экземпляр класса Game.
-  #
-  # Экземпляр ConsoleInterface выводит информацию юзеру. При выводе использует
-  # статические строки типа "У вас осталось ошибок:" и информацию из экземпляра
-  # класса Game, дёргая у него методы, которые мы придумали.
   def initialize(game)
     @game = game
   end
 
-  # Выводит в консоль текущее состояние игры, используя данные из экземпляра
-  # класса Game (количество ошибок, сколько осталось попыток и т.д.)
   def print_out
     puts <<~RESULT_GAME
-      Слово: #{word_to_show}
-      #{figure}
-      Ошибки (#{@game.errors_made}): #{errors_to_show}
+      #{"Слово: #{word_to_show}".colorize(:blue)}
+      #{figure.colorize(:yellow)}
+      #{"Ошибки (#{@game.errors_made}): #{errors_to_show}".colorize(:red)}
       У вас осталось ошибок: #{@game.errors_allowed}
 
     RESULT_GAME
 
     if @game.won?
-      puts "Поздравляем, вы выиграли!"
+      puts "#{"Поздравляем, вы выиграли!".colorize(:green)}"
     elsif @game.lost?
-      puts "Вы проиграли, загаданное слово: #{@game.word}"
+      puts "#{"Вы проиграли, загаданное слово: #{@game.word}".colorize(:red)}"
     end
   end
 
-  # Возвращает ту фигуру из массива FIGURES, которая соответствует количеству
-  # ошибок, сделанных пользователем на данный момент (число ошибок берем у
-  # экземпляра класса Game)
   def figure
     FIGURES[@game.errors_made]
   end
 
-  # Метод, который готовит слово для вывода "на игровом табло".
-  #
-  # На вход передали: ["К", "О", nil, "О", nil, nil],
-  # на выходе будет: "К О __ О __ __"
   def word_to_show
     @game.letters_to_guess.map { |letter| letter || "__" }.join(" ")
   end
 
-  # Получает массив ошибочных букв и склеивает их в строку вида "Х, У"
   def errors_to_show
     @game.errors.join(", ")
   end
 
-  # Получает букву из пользовательского ввода, приводит её к верхнему регистру
-  # и возвращает её
   def get_input
     print "Введите следующую букву:"
     $stdin.gets[0].upcase
--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,4 @@
 class Game
-  # Количество допустимых ошибок
   TOTAL_ERRORS_ALLOWED = 7
 
   def initialize(word)
@@ -7,26 +6,18 @@ class Game
     @user_guesses = []
   end
 
-  # Возвращает массив букв, введенных пользователем, но отсутствующих в
-  # загаданном слове (ошибочные буквы)
   def errors
     @user_guesses - normalized_letters
   end
 
-  # Возвращает количество ошибок, сделанных пользователем
   def errors_made
     errors.length
   end
 
-  # Отнимает от допустимого количества ошибок число сделанных ошибок и
-  # возвращает оставшееся число допустимых ошибок
   def errors_allowed
     TOTAL_ERRORS_ALLOWED - errors_made
   end
 
-  # Возвращает массив с уже отгаданными буквами, вместо неотгаданных букв в
-  # массиве на соответствующем месте находится nil. Этот массив нужен методу
-  # экземпляра класса ConsoleInterface для вывода слова на игровом табло.
   def letters_to_guess
     @letters.map do |letter|
       if @user_guesses.include?(normalize_letter(letter))
@@ -45,8 +36,6 @@ class Game
     won? || lost?
   end
 
-  # По сути, это основной игровой метод, типа "сыграть букву".
-  #
   # Если игра не закончена и передаваемая буква отсутствует в массиве
   # введённых букв, то закидывает передаваемую букву в массив "попыток".
   def play!(letter)
